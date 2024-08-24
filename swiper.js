e90c0d33b3567cdb68bdd910fcc4c4050666e332
nandhuaries <nandhu.s@ariesesolutions.com>
2024-08-24 18:56:43 +0530

    completed the review section

---
+++ swiper.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var swiper = new Swiper(".swiper", {
+    // slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+      },
+
+      480: {
+        slidesPerView: 1,
+        spaceBetween: 30,
+      },
+
+      640: {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
+    },
+  });
+});
