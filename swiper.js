8d8256d7bb32c1dba9d8ec8ed1364674c48f203a
nandhuaries <nandhu.s@ariesesolutions.com>
2024-08-24 20:55:17 +0530

    added autoplay to swiper

--- swiper.js
+++ swiper.js
@@ -2,10 +2,10 @@ document.addEventListener("DOMContentLoaded", function () {
   var swiper = new Swiper(".swiper", {
     centeredSlides: true,
     loop: true,
-    // autoplay: {
-    //   delay: 2000,
-    //   disableOnInteraction: false,
-    // },
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+    },
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
@@ -17,6 +17,7 @@ document.addEventListener("DOMContentLoaded", function () {
     breakpoints: {
       320: {
         slidesPerView: 1,
+        spaceBetween: 30,
       },
 
       //   480: {
